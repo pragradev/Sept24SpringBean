62547025b5d9886ad4903cf59408abe112ded2f4
pragradev <shubh@pragra.io>
2024-10-15 20:15:57 -0400

    initial commit

---
+++ src/main/java/io/pragra/learning/july24springbean/July24SpringBeanApplication.java
@@ -0,0 +1,13 @@
+package io.pragra.learning.july24springbean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class July24SpringBeanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(July24SpringBeanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/pragra/learning/july24springbean/July24SpringBeanApplicationTests.java
@@ -0,0 +1,13 @@
+package io.pragra.learning.july24springbean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class July24SpringBeanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

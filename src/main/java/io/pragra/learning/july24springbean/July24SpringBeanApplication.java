d5e0e4426cefdd1d4c2538c7a21fa3f5a4c33d86
pragradev <shubh@pragra.io>
2024-10-15 21:57:01 -0400

    added multiple bean for same class

--- src/main/java/io/pragra/learning/july24springbean/July24SpringBeanApplication.java
+++ src/main/java/io/pragra/learning/july24springbean/July24SpringBeanApplication.java
@@ -12,8 +12,14 @@ public class July24SpringBeanApplication {
 
         ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
 
-        Employee employee = context.getBean(Employee.class);
+        Employee employee = context.getBean(Employee.class);// Factory design pattern
         System.out.println(employee);
+        DieselEngine dieselEngine = context.getBean(DieselEngine.class);
+        BMW m5 = context.getBean("m5",BMW.class);
+        BMW m3 = context.getBean("m3",BMW.class);
+        System.out.println("M5 = " + m5);
+        System.out.println("M3 = " + m3);
+        System.out.println(dieselEngine);
 
 //        BMW m4 = new BMW("M4", "2024", 450, new DieselEngine(550));
 //        System.out.println(m4);
